ab69687c3d1d4eca72af170058c7dbf63abacb6a
Andrey <andreybespaliy69@gmail.com>
2024-12-12 13:08:28 +0200

    build: add sass, basic styles, Inter font
    - add sass, basic styles: variables, reset, global
    - add global.d.ts
    - add Inter font

--- eslint.config.js
+++ eslint.config.js
@@ -8,57 +8,57 @@ import prettierConfig from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config(
-  {
-    ignores: ['dist', 'build', 'node_modules', 'eslint.config.js'],
-  },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx,js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2024,
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.es2024,
-      },
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
+    {
+        ignores: ['dist', 'build', 'node_modules', 'eslint.config.js', 'src/global.d.ts'],
+    },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx,js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2024,
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.es2024,
+            },
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
         },
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier: prettierPlugin,
-    },
-    rules: {
-      // react
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        {
-          allowConstantExport: true,
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            prettier: prettierPlugin,
         },
-      ],
+        rules: {
+            // react
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                {
+                    allowConstantExport: true,
+                },
+            ],
+        },
     },
-  },
-  {
-    rules: {
-      'prettier/prettier': 'error',
-      // options
-      'no-console': 'warn',
-      'no-debugger': 'error',
-      'no-undef': 'error',
-      'no-extra-semi': 'error',
-      'no-empty': ['error', { allowEmptyCatch: true }],
-      'no-unreachable': 'error',
-      'prefer-const': 'error',
-      'no-unused-vars': 'off',
+    {
+        rules: {
+            'prettier/prettier': 'error',
+            // options
+            'no-console': 'warn',
+            'no-debugger': 'error',
+            'no-undef': 'error',
+            'no-extra-semi': 'error',
+            'no-empty': ['error', { allowEmptyCatch: true }],
+            'no-unreachable': 'error',
+            'prefer-const': 'error',
+            'no-unused-vars': 'off',
+        },
     },
-  },
-  prettierConfig,
+    prettierConfig,
 );
