76f025704059d29fe3e636add58b944bd1735877
Andrey <andreybespaliy69@gmail.com>
2024-12-12 10:10:10 +0200

    build: update eslint, add prettier
    - add prettier
    - add eslint-config-prettier
    - add eslint-plugin-prettier
    - update eslint from 9.15.0 to 9.16.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,64 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+// prettier
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  {
+    ignores: ['dist', 'build', 'node_modules', 'eslint.config.js'],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2024,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2024,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
     },
     rules: {
+      // react
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        {
+          allowConstantExport: true,
+        },
       ],
     },
   },
-)
+  {
+    rules: {
+      'prettier/prettier': 'error',
+      // options
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'no-undef': 'error',
+      'no-extra-semi': 'error',
+      'no-empty': ['error', { allowEmptyCatch: true }],
+      'no-unreachable': 'error',
+      'prefer-const': 'error',
+      'no-unused-vars': 'off',
+    },
+  },
+  prettierConfig,
+);
